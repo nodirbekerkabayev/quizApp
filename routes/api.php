a06de973f8802199ca977f53590ab0889b3a2eda
Dilshodbek20050619 <erolovdilshod019@gmail.com>
2025-01-06 13:34:10 +0500

    implement bearer token

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use Source\Router;
 use App\Controllers\API\UserController;
 
 Router::post('/api/users', [UserController::class , 'store']);
+Router::get('/api/users/{id}', [UserController::class , 'show']);
+Router::delete('/api/users/{id}', [UserController::class , 'destroy']);
+Router::put('/api/users/{id}', [UserController::class , 'update']);
