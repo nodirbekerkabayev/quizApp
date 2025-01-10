4f1c11fda8add89d356a7c98d89f08154a952bfd
nodirbekerkabayev <erkabayevnodirbek@gmail.com>
2025-01-10 17:29:39 +0500

    Reverted to specific commit and adjusted code

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,5 @@ Router::get('/about', [HomeController::class, 'about']);
 Router::get('/login', [HomeController::class, 'login']);
 Router::get('/register', [HomeController::class, 'register']);
 Router::get('/dashboard', [UserController::class, 'home']);
+
 Router::notFound();
