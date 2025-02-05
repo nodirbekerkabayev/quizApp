fd5c8a857d54717a2213831588200a19b2c8503d
nodirbekerkabayev <erkabayevnodirbek@gmail.com>
2025-02-05 20:13:06 +0500

    change header

--- resources/views/components/dashboard/header.php
+++ resources/views/components/dashboard/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us - Quiz App</title>
-    <link rel="stylesheet" href="<?php echo assets('/css/tailwind.css') ?>">
+    <!--<link rel="stylesheet" href="<?php assets('/css/tailwind.css') ?>">-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
